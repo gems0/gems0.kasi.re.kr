c57a23519b281e7cacaa680606690d6f4b91d004
Jae-Joon Lee <lee.j.joon@gmail.com>
2014-06-19 22:57:28 +0900

    table page now support filtering

--- public/js/table/list.js
+++ public/js/table/list.js
@@ -34,6 +34,47 @@ tableListApp.factory('tableListFactory', function($http) {
     return obj;
 });
 
+function tableRootCtrl($scope, $http) {
+    $scope.do_query = function() {
+	$http.jsonp("http://gems0-uwife.appspot.com/query/jsonp?callback=JSON_CALLBACK&coord=gal&lon=" + $scope.lon + "&lat=" + $scope.lat + "&width=" + $scope.size).success(
+	    function(data) {
+		$scope.filtered_list = data;
+	    }
+	);
+    };
+
+    $scope.clear_query = function() {
+	$scope.lon = "";
+	$scope.lat = "";
+	$scope.size = "";
+	$scope.filtered_list = undefined;
+    };
+
+    $scope.filter_msbid = function(a) {
+	if ($scope.filtered_list) {
+	    var msbid_list = _.map($scope.filtered_list.value, 
+				   function(row){ return row.msbid; });
+	    $scope.msbid_list = msbid_list;
+	    var filtered_list = $.grep(a, function(v) {
+		return $.inArray(v[0], msbid_list) > -1;
+	    });
+	    return filtered_list;
+	} else {
+	    return a;
+	}
+    }
+}
+
+// tableListApp.filter('filtermsbid', function() {
+//   return function(input, msbid) {
+//     return input ? '\u2713' : '\u2718';
+//   };
+// });
+
+// var found_names = $.grep(names, function(v) {
+//     return v.name === "Joe" && v.age < 30;
+// });
+
 function tableListCtrl($scope, $location, tableListFactory) {
     $scope.table_summary =  tableListFactory;
     // $scope.uwife_id = $routeParams.uwife_id;
